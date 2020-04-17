6fc8eccb0fdda87945f9c7e46c22b55cd03bcf11
RemmyDZ <remco.dezeeuw@upcmail.nl>
2020-04-17 12:28:03 +0200

    Added dummy questions

--- SeriousGameMinorDTE/globals.h
+++ SeriousGameMinorDTE/globals.h
@@ -9,3 +9,12 @@ const int FPS = 60;
 
 //Main loop condition
 bool isGameFinished = false; //Main loop will break once this turns 'true'
+
+//Questions
+const char* questions[] = { "Question 1", "Question 2", "Question 3", "Question 4" };
+
+//Answers
+const char* answers[][4] = { {"A", "B", "C", "D"}, //Maximum of 4 answers for a single question
+							{"AA", "BB", "CC", "DD"},
+							{"AAA", "BBB", "CCC", "DDD"},
+							{"AAAA", "BBBB", "CCCC", "DDDD"} };
