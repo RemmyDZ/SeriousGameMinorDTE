45b3aaae3730314ac5045e0a4e2b56a9a55cb1d0
RemmyDZ <remco.dezeeuw@upcmail.nl>
2020-04-17 13:27:54 +0200

    Updated Question object

--- SeriousGameMinorDTE/objects.h
+++ SeriousGameMinorDTE/objects.h
@@ -3,7 +3,20 @@
 //Question object
 struct Question {
 	int x, y;
-	
+	char* question;
+	ALLEGRO_BITMAP* bitmap;
+
+	Question(int x, int y, char* question) //Constructor
+	{
+		this->x = x;
+		this->y = y;
+		this->question = question;
+	}
+
+	void setNextQuestion()
+	{
+		//this->question =
+	}
 };
 
 struct Answer {
